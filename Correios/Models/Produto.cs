48185fb2e8535a87ed7e699e85e3b2620b54097d
marcelohfp <marcelo.paes142@gmail.com>
2024-03-21 17:41:37 -0300

    Adicionar arquivos de projeto.

---
+++ Correios/Models/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Site.test.Models
+{
+    [Table("produto")]
+    public class Produto
+    {
+        [Display(Name = "Codigo")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Marca")]
+        [Column("Marca")]
+        public string Marca { get; set; }
+
+        [Display(Name = "Data")]
+        [Column("Data")]
+        public string Data { get; set; }
+
+        [Display(Name = "Destinatario")]
+        [Column("Destinatario")]
+        public string Destinatario { get; set; }
+
+        [Display(Name = "Destino")]
+        [Column("Destino")]
+        public string Destino { get; set; }
+    }
+}
---
+++ Correios/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Site.test.Models.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseMySql(
+        "server=localhost;initial catalog=CRUD_CORREIOS;uid=root;pwd=Join123#@!Bike",
+        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.36-mysql")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Produtoes}/{action=Index}/{id?}");
+
+app.Run();
